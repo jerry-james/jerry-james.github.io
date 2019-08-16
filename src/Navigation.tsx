9d1e434e04f21151e3ec4e7f8a3180f85376d9bb
Jerry James <jerry.james@microchip.com>
2019-08-16 13:44:47 -0700

    ...

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import {connect} from "react-redux";
 import {State} from "./State";
 import {Action} from "redux";
+import {Home} from "./Home";
+import {Blog} from "./Blog";
+
 
 export type NavigationState = string;
 interface NavigationAction extends Action<string> { target: string };
@@ -10,16 +13,31 @@ const type = 'NAVIGATE';
 interface Props {
     style:  React.CSSProperties;
     onClick: (event :React.MouseEvent<HTMLButtonElement>)  => void;
+    content: NavigationState;
+}
+
+const pages : {
+    [index: string]: any
+} = {
+    Home: <Home />,
+    Blog: <Blog />
+};
+
+function Pages(props: { content: string }) {
+    return pages[props.content];
 }
 
 export class NavigationComponent extends React.Component<Props> {
     render() {
+
         return <>
             <div style={this.props.style}>
                 <h2>Navigation</h2>
-                <button onClick={this.props.onClick}>Home</button>
-                <button onClick={this.props.onClick}>Blog</button>
+                {Object.keys(pages).map((value, index) => {
+                    return <button key={index} onClick={this.props.onClick}>{value}</button>;
+                })}
             </div>
+            <Pages content={this.props.content}/>
         </>;
     }
 }
@@ -32,7 +50,8 @@ export const navigationReducer =
 
 function mapStateToProps(state: State) {
     return {
-        style: state.style.navigation
+        style: state.style.navigation,
+        content: state.nav.toString()
     }
 }
 
