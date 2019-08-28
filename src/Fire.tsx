edf0c23f53408d46c1e80f4468187e27eb6da211
Jerry James <jerryj@g.clemson.edu>
2019-08-27 23:06:05 -0700

    Moves some stuff out of the renderer

--- src/Fire.tsx
+++ src/Fire.tsx
@@ -16,6 +16,51 @@ interface Props {
     step: () => void;
 }
 
+
+let v0 = new Vector3(0.0, 0.0, 0.0);
+let v1 = new Vector3(Math.cos(1*(Math.PI/3)), Math.sin(1*(Math.PI/3)), 0.0);
+let v2 = new Vector3(Math.cos(2*(Math.PI/3)), Math.sin(2*(Math.PI/3)), 0.0);
+let v3 = new Vector3(Math.cos(3*(Math.PI/3)), Math.sin(3*(Math.PI/3)), 0.0);
+let v4 = new Vector3(Math.cos(4*(Math.PI/3)), Math.sin(4*(Math.PI/3)), 0.0);
+let v5 = new Vector3(Math.cos(5*(Math.PI/3)), Math.sin(5*(Math.PI/3)), 0.0);
+let v6 = new Vector3(Math.cos(6*(Math.PI/3)), Math.sin(6*(Math.PI/3)), 0.0);
+let v7 = new Vector3(Math.cos(7*(Math.PI/3)), Math.sin(7*(Math.PI/3)), 0.0);
+
+let vertices = new Float32Array(
+    [
+        ...v0.elements(),
+        ...v1.elements(),
+        ...v2.elements(),
+        ...v3.elements(),
+        ...v4.elements(),
+        ...v5.elements(),
+        ...v6.elements(),
+        ...v7.elements()
+    ]);
+
+
+let viewMatrix = new Matrix4();
+let yy = 50;
+let xx = -5;
+viewMatrix.setLookAt(xx,
+                     yy,
+                     450.00,
+                     xx, yy, 0,
+                     0 , 1, 0);
+
+
+
+let projMatrix = new Matrix4();
+projMatrix.setPerspective(30, WIDTH/HEIGHT, 1, 1000);
+
+let HEX_BASIS = new Matrix4().set(new Float32Array(
+    [
+        -1.5,Math.sqrt(3/4),0,0,
+        +1.5,Math.sqrt(3/4),0,0,
+        0,0,1,0,
+        0,0,0,1]));
+
+
 export class FireComponent extends React.Component<Props> {
     private readonly canvas: React.RefObject<HTMLCanvasElement>;
     private gl: WebGLRenderingContext | null;
@@ -63,40 +108,6 @@ export class FireComponent extends React.Component<Props> {
     }
 
     renderWebgl() {
-        let v0 = new Vector3(0.0, 0.0, 0.0);
-        let v1 = new Vector3(Math.cos(1*(Math.PI/3)), Math.sin(1*(Math.PI/3)), 0.0);
-        let v2 = new Vector3(Math.cos(2*(Math.PI/3)), Math.sin(2*(Math.PI/3)), 0.0);
-        let v3 = new Vector3(Math.cos(3*(Math.PI/3)), Math.sin(3*(Math.PI/3)), 0.0);
-        let v4 = new Vector3(Math.cos(4*(Math.PI/3)), Math.sin(4*(Math.PI/3)), 0.0);
-        let v5 = new Vector3(Math.cos(5*(Math.PI/3)), Math.sin(5*(Math.PI/3)), 0.0);
-        let v6 = new Vector3(Math.cos(6*(Math.PI/3)), Math.sin(6*(Math.PI/3)), 0.0);
-        let v7 = new Vector3(Math.cos(7*(Math.PI/3)), Math.sin(7*(Math.PI/3)), 0.0);
-
-        let vertices = new Float32Array(
-            [
-                ...v0.elements(),
-                ...v1.elements(),
-                ...v2.elements(),
-                ...v3.elements(),
-                ...v4.elements(),
-                ...v5.elements(),
-                ...v6.elements(),
-                ...v7.elements()
-            ]);
-        let viewMatrix = new Matrix4();
-        let yy = 50;
-        let xx = -45;
-        viewMatrix.setLookAt(xx,
-                             yy,
-                             450.00,
-                             xx, yy, 0,
-                             0 , 1, 0);
-
-
-
-        let projMatrix = new Matrix4();
-        projMatrix.setPerspective(30, WIDTH/HEIGHT, 1, 1000);
-
         if(this.gl && this._loader.program) {
             let gl = this.gl;
             gl.enable(gl.DEPTH_TEST);
@@ -108,17 +119,8 @@ export class FireComponent extends React.Component<Props> {
             let u_viewMatrix = gl.getUniformLocation(this._loader.program, 'u_ViewMatrix');
             let u_projMatrix = gl.getUniformLocation(this._loader.program, 'u_ProjMatrix');
 
-            viewMatrix.rotate(30,0,0,1);
-
             gl.uniformMatrix4fv(u_projMatrix, false, projMatrix.elements);
             gl.uniformMatrix4fv(u_viewMatrix, false, viewMatrix.elements);
-
-            let HEX_BASIS = new Matrix4().set(new Float32Array(
-                [
-                    -1.5,Math.sqrt(3/4),0,0,
-                    +1.5,Math.sqrt(3/4),0,0,
-                    0,0,1,0,
-                    0,0,0,1]));
 
             let vertexBuffer = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
